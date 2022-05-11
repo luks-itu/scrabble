8411e5bb8bcb835ac7110dbb9e3fd4bce8d00d86
“luks-itu” <luks@itu.dk>
2022-05-11 16:32:05 +0200

    getting there

--- ScrabbleTemplate/ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/ScrabbleTemplate/Program.fs
@@ -20,15 +20,15 @@ let spawnMultiples name dict bot =
 
 [<EntryPoint>]
 let main argv =
-    ScrabbleUtil.DebugPrint.toggleDebugPrint true // Change to false to supress debug output
+    ScrabbleUtil.DebugPrint.toggleDebugPrint false // Change to false to supress debug output
 
     System.Console.BackgroundColor <- System.ConsoleColor.White
     System.Console.ForegroundColor <- System.ConsoleColor.Black
     System.Console.Clear()
 
 
-    let board        = ScrabbleUtil.StandardBoard.standardBoard ()
-//    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
+//    let board        = ScrabbleUtil.StandardBoard.standardBoard ()
+    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard ()
 
 //    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
 //    let board      = ScrabbleUtil.RandomBoard.randomBoardSeed (Some 42)
@@ -56,7 +56,7 @@ let main argv =
     // Uncomment this line to call your client
     let players =
         [("FSkarpBot", dictionary, FSkarp.Scrabble.startGame);
-        ]
+        ("OBBot", dictionary, Oxyphenbutazone.Scrabble.startGame);]
     //let players =
     //("OBBot", dictionary, Oxyphenbutazone.Scrabble.startGame)
     //  spawnMultiples "FSkarpBot" dictionary FSkarp.Scrabble.startGame 2
